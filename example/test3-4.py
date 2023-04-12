06db455792cf24df401026f1141cd71225653177
ywdblog <ywdblog@gmail.com>
2023-04-12 19:13:39 +0800

    理解层

--- example/test3-4.py
+++ example/test3-4.py
@@ -1,10 +1,47 @@
 # -*- coding: utf-8 -*
 # Anatomy of a neural network: Understanding core Keras APIs
+## Layers: The building blocks of deep learning
+### The base Layer class in Keras
+#### A Dense layer implemented as a Layer subclass
 
-import matplotlib.pyplot as plt
+from tensorflow import keras
 import tensorflow as tf
-import numpy as np
-import sys
+import sys 
+
+class SimpleDense(keras.layers.Layer):
+
+    def __init__(self, units, activation=None):
+        super().__init__()
+        self.units = units
+        self.activation = activation
+
+    # 权重一般是在build方法中创建的
+    def build(self, input_shape):
+        # -1表示最后一个维度
+        input_dim = input_shape[-1]
+        print("input_shape",input_shape)
+
+        # add_weight()方法创建权重 shape=(input_dim, self.units)表示权重的形状 initializer="random_normal"表示权重的初始化方式
+        # self.W <tf.Variable 'simple_dense/Variable:0' shape=(784, 32) dtype=float32
+        # shape=(784, 32) 表示权重的形状 784表示输入的维度 32表示输出的维度
+        self.W = self.add_weight(shape=(input_dim, self.units),
+                                 initializer="random_normal")
+        
+        print("self.W",self.W)
+         
+        self.b = self.add_weight(shape=(self.units,),
+                                 initializer="zeros")
+
+    def call(self, inputs):
+        y = tf.matmul(inputs, self.W) + self.b
+        if self.activation is not None:
+            y = self.activation(y)
+        return y
 
 
-tf.nn.relu()
+my_dense = SimpleDense(units=32, activation=tf.nn.relu)
+# input_tensor 2表示批量大小 784表示输入的维度
+input_tensor = tf.ones(shape=(2, 784))
+
+output_tensor = my_dense(input_tensor)
+print(output_tensor.shape) # (2,32)
---
+++ example/test3-5.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*
+# Anatomy of a neural network: Understanding core Keras APIs
+## Layers: The building blocks of deep learning
+### The base Layer class in Keras
+#### Automatic shape inference: Building layers on the fly
+
+from tensorflow import keras
+import tensorflow as tf
+import sys 
+
+import numpy as np
+ 
+
+class SimpleDense(keras.layers.Layer):
+
+    def __init__(self, units, activation=None):
+        super().__init__()
+        self.units = units
+        #self.activation = activation
+        self.activation = keras.activations.get(activation)
+ 
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+        self.W = self.add_weight(shape=(input_dim, self.units),
+                                 initializer="random_normal")
+        self.b = self.add_weight(shape=(self.units,),
+                                 initializer="zeros")
+
+    def call(self, inputs):
+        y = tf.matmul(inputs, self.W) + self.b
+        if self.activation is not None:
+            y = self.activation(y)
+        return y
+    
+    def __call__(self, inputs):
+        if not self.built:
+            self.build(inputs.shape)
+            self.built = True
+        return self.call(inputs)
+
+model = keras.Sequential([
+    SimpleDense(32, activation="relu"),
+    SimpleDense(64, activation="relu"),
+    SimpleDense(32, activation="relu"),
+    SimpleDense(10, activation="softmax")
+])
+
+'''
+在test2-11.py：
+model = NaiveSequential([
+    NaiveDense(input_size=28 * 28, output_size=512, activation=tf.nn.relu),
+    NaiveDense(input_size=512, output_size=10, activation=tf.nn.softmax)
+])
+层兼容性：下一层的输入维度必须与上一层的输出维度相同，如果不同，需要在层中指定输入维度
+其实keras中能自己解决这个问题，只需要在第一层中指定输入维度即可，如下：
+layer = layers.Dense(32, activation="relu")
+model = models.Sequential([
+    layers.Dense(32, activation="relu"),
+    layers.Dense(32)
+])
+那么在SimpleDense自己实现怎么做呢？定义一个__call__()方法
+'''
+
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+# Generate some random training data
+train_data = np.random.rand(1000, 10)
+train_labels = np.random.randint(0, 10, size=(1000,))
+train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=10)
+
+# Train the model
+model.fit(train_data, train_labels, epochs=10, batch_size=32)
+
+# Generate some random test data
+test_data = np.random.rand(100, 10)
+test_labels = np.random.randint(0, 10, size=(100,))
+test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=10)
+
+# Evaluate the model on the test data
+test_loss, test_acc = model.evaluate(test_data, test_labels)
+
+# Print the test loss and accuracy
+print('Test loss:', test_loss)
+print('Test accuracy:', test_acc)
